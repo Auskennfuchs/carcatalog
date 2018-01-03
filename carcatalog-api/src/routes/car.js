d9f0823e1dd835a493d0648c95d33f115566a0df
Auskennfuchs <micschilling@hotmail.com>
2018-01-03 23:30:37 +0100

    update routes

--- carcatalog-api/src/routes/car.js
+++ carcatalog-api/src/routes/car.js
@@ -8,28 +8,6 @@ router.use((req, res, next) => {
     next()
 })
 
-router.get('/', (req, res) => {
-    res.json({ message: "läuft" })
-})
-
-router.post('/cars', (req, res) => {
-    var car = new Car()
-    car.name = req.body.name
-    car.manufacture = req.body.manufacture
-    car.powerPS = req.body.powerPS
-    car.powerKW = req.body.powerKW
-    car.shape = req.body.shape
-    car.hsn = req.body.hsn
-    car.tsn = req.body.tsn
-
-    car.save((err) => {
-        if (err) {
-            res.send(err)
-        }
-        res.json({ message: "Car created" })
-    })
-})
-
 router.route('/cars')
     .get((req, res) => {
         Car.find((err, cars) => {
@@ -37,6 +15,23 @@ router.route('/cars')
                 res.send(err)
             }
             res.json(cars)
+        })
+    })
+    .post((req, res) => {
+        var car = new Car()
+        car.name = req.body.name
+        car.manufacture = req.body.manufacture
+        car.powerPS = req.body.powerPS
+        car.powerKW = req.body.powerKW
+        car.shape = req.body.shape
+        car.hsn = req.body.hsn
+        car.tsn = req.body.tsn
+
+        car.save((err) => {
+            if (err) {
+                res.send(err)
+            }
+            res.json({ message: "Car created" })
         })
     })
 
